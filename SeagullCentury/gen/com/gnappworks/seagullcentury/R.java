1de4e7d67470487d1512daafdd32622aa4e3d17e
Joshua Souders <rem87062@gmail.com>
2014-02-18 12:20:00 -0500

    fixed bug, now all tabs load from their own layout file

--- SeagullCentury/gen/com/gnappworks/seagullcentury/R.java
+++ SeagullCentury/gen/com/gnappworks/seagullcentury/R.java
@@ -26,10 +26,10 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080004;
-        public static final int button1=0x7f080002;
+        public static final int course_map_button=0x7f080001;
+        public static final int home_button=0x7f080002;
         public static final int pager=0x7f080000;
-        public static final int section_label=0x7f080001;
-        public static final int su_website_button=0x7f080003;
+        public static final int su_map_button=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
