63f0822ad92194256ea3bb3f8fea4617aac3432a
Tyler Frost <tylerfrost64@gmail.com>
2014-02-28 22:43:06 -0500

    Update CourseMapFragment.java
    Added webview within the CourseMapFragment to open Leaflet  map

--- SeagullCentury/src/com/gnappworks/seagullcentury/CourseMapFragment.java
+++ SeagullCentury/src/com/gnappworks/seagullcentury/CourseMapFragment.java
@@ -5,15 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.content.res.AssetManager;
 
-public class CourseMapFragment extends Fragment {
-	public CourseMapFragment() {
-	}
+public class CourseMapFragment extends Fragment 
+{
+	public CourseMapFragment() {}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.course_map_fragment, container, false);
-
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
+	{
+		View rootView = (View) inflater.inflate(R.layout.course_map_fragment, container, false);
+		WebView webView= (WebView) rootView.findViewById(R.id.webview);
+		webView.loadUrl("file:///android_asset/courseMap.html");
 		return rootView;
 	}
 
