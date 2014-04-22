5facf3cd42e30e7b7b9cc27085ebbd777fb9bebc
Joshua Souders <rem87062@gmail.com>
2014-04-22 12:22:03 -0400

    added geolocation functionality, fixed URL

--- SeagullCentury/src/com/gnappworks/seagullcentury/CourseMapFragment.java
+++ SeagullCentury/src/com/gnappworks/seagullcentury/CourseMapFragment.java
@@ -2,12 +2,12 @@ package com.gnappworks.seagullcentury;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.GeolocationPermissions;
 
 public class CourseMapFragment extends Fragment 
 {
@@ -19,11 +19,19 @@ public class CourseMapFragment extends Fragment
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
 	{
 		String url;
-		url = "http://apps.esrgc.org/maps/seagullcentury/index.html?route="+HomeFragment.routeNumber;
+		url = "http://oxford.esrgc.org/maps/seagullcentury/index.html?route="+HomeFragment.routeNumber;
 		View rootView = (View) inflater.inflate(R.layout.course_map_fragment, container, false);
 		webView= (WebView) rootView.findViewById(R.id.webview);
 		webView.getSettings().setJavaScriptEnabled(true);
-	    webView.setWebChromeClient(new WebChromeClient());
+	    webView.setWebChromeClient(new WebChromeClient(){
+	    	//Default behavior is that the "Do you want to use location?" doesn't pop up in a WebView. This
+	    	//overrides it and makes that box pop up.
+	    	public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+	    		callback.invoke(origin, true, false);
+	    	}
+	    });
+	    //Sets the geolocation database path which allows for location caching
+	    webView.getSettings().setGeolocationDatabasePath(getActivity().getFilesDir().getPath() );
 	    webView.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
 		webView.loadUrl(url);
 		return rootView;
@@ -33,7 +41,7 @@ public class CourseMapFragment extends Fragment
 	{
 	    super.onResume();
 	    String url;
-		url = "http://apps.esrgc.org/maps/seagullcentury/index.html?route="+HomeFragment.routeNumber;
+		url = "http://oxford.esrgc.org/maps/seagullcentury/index.html?route="+HomeFragment.routeNumber;
 	    webView.loadUrl(url);
 	}
 	public void onPause() 
